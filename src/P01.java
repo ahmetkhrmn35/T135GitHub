81035895db9eb8004b4ce86ff953ca08c716e375
Ahmet <ahmetkhrmn7654321@gmail.com>
2024-02-29 19:05:54 +0300

    yeni20240229

--- src/P01.java
+++ src/P01.java
@@ -4,7 +4,9 @@ public class P01 {
         System.out.println("Hello world");
         int a = 120;
         int b = 80;
-
+        int c ;
+        c=a+b;
+        System.out.println("c = " + c);
 
     }
 }
---
+++ src/P03.java
@@ -0,0 +1,7 @@
+public class P03 {
+    public static void main(String[] args) {
+
+        System.out.println("Versiyon yenileme");
+
+    }
+}
